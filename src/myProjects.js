6943e4def6c87d56c86abba96a73ba3fa09ea9c8
João <joao.pdesa@hotmail.com>
2021-10-04 00:32:44 -0300

    alt

--- src/myProjects.js
+++ src/myProjects.js
@@ -1,6 +1,7 @@
 const myProjects = [{
     id: 'project1',
     img: 'img/barbershop.jpg',
+    alt: 'site de barbearia',
     pagelink: 'https://barbershopfirstfreela.netlify.app/',
     repositorylink: 'https://github.com/JoaoP-Sa/barbershop',
     projectdescription: "Primeiro projeto que fiz por conta própria, é uma landing page com tema de barbearia que criei utilizando reactjs e possui google maps integrado."
@@ -8,18 +9,21 @@ const myProjects = [{
 {
     id: 'project2',
     img: 'img/sunnyside.jpg',
+    alt: 'site de serviços de design',
     pagelink: 'https://joaop-sa.github.io/sunnyside/',
     repositorylink: 'https://github.com/JoaoP-Sa/sunnyside',
     projectdescription: "Este link contém uma página que encontrei em um desafio do frontend mentor e decidi recriar utilizando html, css e javascript."
 },{
     id: 'project3',
     img: 'img/analythics.jpg',
+    alt: 'site de empresa de análise de dados',
     pagelink: 'https://joaop-sa.github.io/analythics_card/',
     repositorylink: 'https://github.com/JoaoP-Sa/analythics_card',
     projectdescription: "Outra página que criei com base em um desafio do front end mentor, é uma página para uma empresa que trabalha com análise de dados. Utilizei html e css para criar a página."
 },{
     id: 'project4',
     img: 'img/intro-component.jpg',
+    alt: 'site de página de login',
     pagelink: 'https://jp-intro-component.netlify.app/',
     repositorylink: 'https://github.com/JoaoP-Sa/intro-component',
     projectdescription: "Essa página consiste em uma tela de login de um site de cursos onde o usuário pode acompanhar outros programadores desenvolvendo seus projetos. Utilizei apenas react js e css para criar este projeto."
